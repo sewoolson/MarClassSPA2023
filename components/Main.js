55d81f16bac2fd04f3e1159810e8ca6f4cc9e610
sewoolson <samwoolson@gmail.com>
2023-04-25 21:39:21 -0500

    components complete

---
+++ components/Main.js
@@ -0,0 +1,11 @@
+import html from "html-literal";
+import * as views from "./views";
+
+
+export default () => html`
+  ${views["Home"]()},
+  ${views["Bio"]()},
+  ${views["Pizza"]()},
+  ${views["Order"]()}
+`;
+
