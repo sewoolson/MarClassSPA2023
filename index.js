8947f8709cbad326a3cf2f8938cad020ee31f794
sewoolson <samwoolson@gmail.com>
2023-05-04 15:45:07 -0500

    05/04 versions --most recent

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import * as store from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
 import axios from "axios";
+import dotenv from "dotenv";
 
+// Make sure that dotenv.config(); is placed after all of you import statements
+dotenv.config();
 const router = new Navigo("/");
 
 function render(state = store.Home) {
@@ -14,61 +17,65 @@ function render(state = store.Home) {
     ${Footer()}
   `;
 
-afterRender(state);
+  afterRender(state);
 
-router.updatePageLinks();
+  router.updatePageLinks();
 }
 
 function afterRender(state) {
-//add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+  // add menu toggle to bars icon in nav bar
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
 }
 
 router.hooks({
   before: (done, params) => {
-    const view = params && params.data && params.data.view ? capitalize(params.data.view) : "Home";
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
     // Add a switch case statement to handle multiple routes
     switch (view) {
       case "Pizza":
         // New Axios get request utilizing already made environment variable
         axios
-          .get(`https://sc-pizza-api.onrender.com/pizzas`)
-          .then(response => {
+          .get(`${process.env.PIZZA_PLACE_API_URL}/pizzas`)
+          .then((response) => {
             // We need to store the response to the state, in the next step but in the meantime let's see what it looks like so that we know what to store from the response.
             console.log("response", response);
             store.Pizza.pizzas = response.data;
-
             done();
           })
           .catch((error) => {
             console.log("It puked", error);
             done();
           });
-          break;
-      default :
+        break;
+      default:
         done();
     }
   },
   already: (params) => {
-    const view = params && params.data && params.data.view ? capitalize(params.data.view) : "Home";
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
 
     render(store[view]);
-  }
+  },
 });
-
-
 router
   .on({
     "/": () => render(),
-    ":view": params => {
+    ":view": (params) => {
       let view = capitalize(params.data.view);
-      if (store.hasOwnProperty(view)) {
+      if (view in store) {
         render(store[view]);
       } else {
         console.log(`View ${view} not defined`);
+        render(store.Viewnotfound);
       }
-    }
+    },
   })
   .resolve();
