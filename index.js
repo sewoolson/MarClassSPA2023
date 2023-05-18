9d0308b5285594bb492500a69b68bcdeb58f58ce
sewoolson <samwoolson@gmail.com>
2023-05-18 17:37:16 -0500

    new

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
 import Navigo from "navigo";
-import { capitalize } from "lodash";
+import { after, capitalize } from "lodash";
 import axios from "axios";
-
-
+import dotenv from "dotenv";
+// Make sure that dotenv.config(); is placed after all of you import statements
+dotenv.config();
 const router = new Navigo("/");
-
 function render(state = store.Home) {
   document.querySelector("#root").innerHTML = `
     ${Header(state)}
@@ -14,27 +14,54 @@ function render(state = store.Home) {
     ${Main(state)}
     ${Footer()}
   `;
-
-  afterRender(state);
-
+  afterRedner(state);
   router.updatePageLinks();
 }
-
-function afterRender(state) {
-  // add menu toggle to bars icon in nav bar
+function afterRedner(state) {
+  //add menu toggle to bars icon in nav bar
   document.querySelector(".fa-bars").addEventListener("click", () => {
     document.querySelector("nav > ul").classList.toggle("hidden--mobile");
   });
+  if (state.view === "Order") {
+    document.querySelector("form").addEventListener("submit", event => { //grabs the form, listens for submit button
+      event.preventDefault();//prevent the default action aka redirect to the same url using POST method
+      const inputList = event.target.elements;
+      console.log("Input Element List", inputList);
+      const toppings = [];
+      // Interate over the toppings input group elements
+      for (let input of inputList.toppings) {
+        // If the value of the checked attribute is true then add the value to the toppings array
+        if (input.checked) { //if toppings checkbox is selected
+          toppings.push(input.value);
+        }
+      }
+      const requestData = {
+        customer: inputList.customer.value,
+        crust: inputList.crust.value,
+        cheese: inputList.cheese.value,
+        sauce: inputList.sauce.value,
+        toppings: toppings
+      };
+      console.log("request Body", requestData);
+      axios
+        .post(`${process.env.PIZZA_PLACE_API_URL}/pizzas`, requestData)
+        .then(response => {
+          // Push the new pizza onto the Pizza state pizzas attribute, so it can be displayed in the pizza list
+          store.Pizza.pizzas.push(response.data);
+          router.navigate("/Pizza");
+        })
+        .catch(error => {
+          console.log("It puked", error);
+        });
+    });
+  }
 }
-
 router.hooks({
   before: (done, params) => {
-    const view =
-      params && params.data && params.data.view
-        ? capitalize(params.data.view)
-        : "Home";
+    const view = params && params.data && params.data.view ? capitalize(params.data.view) : "Home"; //happend before the page is loaded
     // Add a switch case statement to handle multiple routes
     switch (view) {
+      // New Case for the Home View
       case "Home":
         axios
           // Get request to retrieve the current weather data using the API key and providing a city name
@@ -42,11 +69,9 @@ router.hooks({
             `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
           )
           .then(response => {
-            console.log(response.data);
             // Convert Kelvin to Fahrenheit since OpenWeatherMap does provide otherwise
             const kelvinToFahrenheit = kelvinTemp =>
               Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
-
             // Create an object to be stored in the Home state from the response
             store.Home.weather = {
               city: response.data.name,
@@ -54,17 +79,16 @@ router.hooks({
               feelsLike: kelvinToFahrenheit(response.data.main.feels_like),
               description: response.data.weather[0].main
             };
-
             // An alternate method would be to store the values independently
             /*
-      store.Home.weather.city = response.data.name;
-      store.Home.weather.temp = kelvinToFahrenheit(response.data.main.temp);
-      store.Home.weather.feelsLike = kelvinToFahrenheit(response.data.main.feels_like);
-      store.Home.weather.description = response.data.weather[0].main;
-      */
+            store.Home.weather.city = response.data.name;
+            store.Home.weather.temp = kelvinToFahrenheit(response.data.main.temp);
+            store.Home.weather.feelsLike = kelvinToFahrenheit(response.data.main.feels_like);
+            store.Home.weather.description = response.data.weather[0].main;
+            */
             done();
           })
-          .catch(err => {
+          .catch((err) => {
             console.log(err);
             done();
           });
@@ -73,13 +97,13 @@ router.hooks({
         // New Axios get request utilizing already made environment variable
         axios
           .get(`${process.env.PIZZA_PLACE_API_URL}/pizzas`)
-          .then((response) => {
+          .then(response => {
             // We need to store the response to the state, in the next step but in the meantime let's see what it looks like so that we know what to store from the response.
             console.log("response", response);
-            store.Pizza.pizzas = response.data;
+            store.Pizza.pizzas = response.data; //response.data = array of objects
             done();
           })
-          .catch((error) => {
+          .catch((error) => { //if error occurs, catch "catches" error and returns:
             console.log("It puked", error);
             done();
           });
@@ -89,25 +113,20 @@ router.hooks({
     }
   },
   already: (params) => {
-    const view =
-      params && params.data && params.data.view
-        ? capitalize(params.data.view)
-        : "Home";
-
-    render(store[view]);
-  },
+    const view = params && params.data && params.data.view ? capitalize(params.data.view) : "Home";
+    render(store[view]); //difference between alread and before is this line of code - allows us to make api call when page is already loaded
+  }
 });
 router
   .on({
     "/": () => render(),
-    ":view": (params) => {
+    ":view": params => {
       let view = capitalize(params.data.view);
-      if (view in store) {
+      if (store.hasOwnProperty(view)) {
         render(store[view]);
       } else {
         console.log(`View ${view} not defined`);
-        render(store.Viewnotfound);
       }
-    },
+    }
   })
   .resolve();
